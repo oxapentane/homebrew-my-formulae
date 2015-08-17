0cd657974e336ed709b21fb4e33593ff29a9f9a1
Grigory Shipunov <mail@oxapentane.com>
2015-08-17 11:41:36 +0300

    ooops, fix identation mishap

--- input-source-switcher.rb
+++ input-source-switcher.rb
@@ -1,17 +1,17 @@
 class InputSourceSwitcher < Formula
-	homepage "https://github.com/vovkasm/input-source-switcher"
-	url "https://github.com/vovkasm/input-source-switcher/archive/v0.3.tar.gz"
-	sha256 "c4bb51aae132e672c886ed8add7e31300be21569ec3e2867639e6ee23b38d049"
+    homepage "https://github.com/vovkasm/input-source-switcher"
+    url "https://github.com/vovkasm/input-source-switcher/archive/v0.3.tar.gz"
+    sha256 "c4bb51aae132e672c886ed8add7e31300be21569ec3e2867639e6ee23b38d049"
 
-	depends_on "cmake" => :build
+    depends_on "cmake" => :build
 
-	def install
-		system "cmake", ".", *std_cmake_args
-		system "make", "install"
-	end
+    def install
+        system "cmake", ".", *std_cmake_args
+        system "make", "install"
+    end
 
-	test do
-		system "#{bin}/issw", "-l"
-	end
+    test do
+        system "#{bin}/issw", "-l"
+    end
 end
 
